13df9fa4b871e9fdaeee14e2c4a284fad70e95f8
iradukunda murenzi cherubin <cherubinbond@gmail.com>
2024-09-22 22:48:21 +0200

    add new project

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -22,9 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zelqf#^nm=gfu8y2=n^39oo7yr^7=xlpcgcs7s8g5p!l0=fby8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com', 'your-ip-address']
+
+# Security Settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True  # Set this to True if you're using HTTPS
 
 # Application definition
 INSTALLED_APPS = [
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': 'your_db_port',
     }
 }
 
