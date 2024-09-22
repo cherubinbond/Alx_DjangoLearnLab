de02ea7f0bfb4d23518ed7cc19294c880f322d75
iradukunda murenzi cherubin <cherubinbond@gmail.com>
2024-09-22 17:15:50 +0200

    add accounts/ manage.py

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+
+    def create(self, validated_data):
+        user = CustomUser(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        token = Token.objects.get(user=response.data['user'])
+        return Response({'token': token.key})
+
+
+# Create your views here.
