b608c29b07dbe5b7cabbaf3dde37bb74f54c9383
iradukunda murenzi cherubin <cherubinbond@gmail.com>
2024-09-22 19:57:45 +0200

    add changes made

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
-from rest_framework import generics
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from .serializers import UserRegistrationSerializer, UserLoginSerializer  # Import the correct serializers
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer, UserLoginSerializer  # Ensure correct serializers are imported
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer  # Use the correct serializer for user registration
 
-class LoginView(ObtainAuthToken):
+class LoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
@@ -20,5 +21,18 @@ class LoginView(ObtainAuthToken):
             'email': user.email
         })
 
-# Create your views here.
+class FollowUserView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
+    def post(self, request, user_id):
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'message': f'You are now following {user_to_follow.username}'})
+
+class UnfollowUserView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'message': f'You have unfollowed {user_to_unfollow.username}'})
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
+from .views import FeedView
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(r'feed', FeedView, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
