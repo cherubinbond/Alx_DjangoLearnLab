54397e6ec346f1fb25e471c86e667ed195b7c8cd
iradukunda murenzi cherubin <cherubinbond@gmail.com>
2024-09-22 22:17:25 +0200

    add changes

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,8 +5,18 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the field to now when the object is created
-    updated_at = models.DateTimeField(auto_now=True)  # Automatically update the field to now every time the object is saved
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevent duplicate likes
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
