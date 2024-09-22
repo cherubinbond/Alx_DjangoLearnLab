7981a6df9f32251e0202944a273635ea2e59c5ac
iradukunda murenzi cherubin <cherubinbond@gmail.com>
2024-09-22 20:49:36 +0200

    add changes to url

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,14 +1,7 @@
-from django.urls import path, include
-from .views import FeedView
-from rest_framework.routers import DefaultRouter
-
-# Initialize the router
-router = DefaultRouter()
-
-# Register the FeedView with the router
-router.register(r'feed', FeedView, basename='feed')
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls)),  # Include router URLs
-    # Additional explicit paths can be added here if necessary
+    path('feed/', views.get_feed, name='get_feed'),
+    # Existing URL patterns...
 ]
